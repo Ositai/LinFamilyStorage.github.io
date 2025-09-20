df85cdcf10d1d8590549bb60dedff11c2cd3d4c2
Ositai <super92108@gmail.com>
2025-09-20 15:23:37 +0800

    Update common.js

--- LinFamiStorage/common.js
+++ LinFamiStorage/common.js
@@ -5,9 +5,12 @@ function linkToAdd(name){
   return undefined
 }      
 
+// apps script 網址
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbxlKyOkYISMfd95NMtxd-koIEyjldzzDV3wPd2OY5ztN_1h_RWOlnQCM1yVVLrmxFk2/exec'
+
 // 送出物品名比對既有庫存，有回應則呼叫displayResult產生表格
 function formSender(formObject){  
-  fetch(`${scripUrl}?action='search'&keyword=${formObject}`)
+  fetch(`${scriptUrl}?action='search'&keyword=${formObject}`)
     .then(res => res.json())
     .then(tableData => {
       displayResult(tableData)
@@ -24,6 +27,6 @@ const tableHeader = "<th scope='col'>項目名稱</th>"+
                     "<th scope='col'>目標庫存</th>"+
                     "<th scope='col'>有效期限</th>"
 
-// apps script 網址
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbxlKyOkYISMfd95NMtxd-koIEyjldzzDV3wPd2OY5ztN_1h_RWOlnQCM1yVVLrmxFk2/exec'
+
+
 
