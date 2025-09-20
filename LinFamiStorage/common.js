276316f885f1cde716d1a0500051c7fbe56f7a2c
Ositai <super92108@gmail.com>
2025-09-21 00:55:06 +0800

    Update common.js

--- LinFamiStorage/common.js
+++ LinFamiStorage/common.js
@@ -10,8 +10,13 @@ const scriptUrl = 'https://script.google.com/macros/s/AKfycbxlKyOkYISMfd95NMtxd-
 
 // 送出物品名比對既有庫存，有回應則呼叫displayResult產生表格
 function formSender(formObject){  
-  fetch(`${scriptUrl}?action=search&keyword=${encodeURIComponent(formObject)}`)
-    .then(res => res.json())
+  let objName = formObject.value
+  fetch(`${scriptUrl}?action=search&keyword=${objName}`)
+    .then(res => {
+      console.log('進行到res')
+      res.json()
+      console.log('res.json完成)
+    })
     .then(tableData => {
       displayResult(tableData)
       console.log('get call back')
@@ -26,6 +31,7 @@ const tableHeader = "<th scope='col'>項目名稱</th>"+
                     "<th scope='col'>庫存量</th>"+
                     "<th scope='col'>目標庫存</th>"+
                     "<th scope='col'>有效期限</th>"
+
 
 
 
